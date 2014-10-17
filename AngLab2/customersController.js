46e044b6355ca05596554f3d3409929788ec5745
todorvt <todorvt@abv.bg>
2014-10-17 16:07:11 +0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	AngLab1/.idea/workspace.xml

--- AngLab2/customersController.js
+++ AngLab2/customersController.js
@@ -1,4 +1,15 @@
-function customersController($scope,$http) {
-    $http.get("customers.json")
-        .success(function(response) {$scope.names = response;});
-}
+(function() {
+    var app = angular.module("myApp", []);
+    app.controller("customersController", ['$scope', '$http', function($scope, $http) {
+        this.viewInfo = function(obj){
+            var ind = this.names.indexOf(obj);
+            this.names[ind].showInfo = !this.names[ind].showInfo;
+        }
+        curCtrlNames = angular.bind(this, function (val) {
+            this.names = val;
+        });
+        $http.get("customers.json").success(function( response) {
+            curCtrlNames(response);
+        })
+    }]);
+})();
